853b69edfe7d4f56798a876b39b836c8e25fcb72
julio montilla <juliomontilla100@gmail.com>
2019-03-03 20:24:05 -0300

    a

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ process.env.PORT = process.env.PORT || 3000
 if(process.env.NODE_ENV === 'dev'){
     process.env.URL_DB = 'mongodb://localhost:27017/crud'
 }else {
-    process.env.URL_DB = 'mongodb+srv://juliomontilla100:Maracaibo123.@crud-node-mongo-ejs-zgddq.mongodb.net/test?retryWrites=true'
+    process.env.URL_DB = 'mongodb://juliomontilla100:04186131441@crud-node-mongo-ejs-shard-00-00-zgddq.mongodb.net:27017,crud-node-mongo-ejs-shard-00-01-zgddq.mongodb.net:27017,crud-node-mongo-ejs-shard-00-02-zgddq.mongodb.net:27017/test?ssl=true&replicaSet=crud-node-mongo-ejs-shard-0&authSource=admin&retryWrites=true'
 }
